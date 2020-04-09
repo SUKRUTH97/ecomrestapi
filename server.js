165cefc65d5d125520c527fe8fc13a838cd9d123
SUKRUTH97 <60535791+SUKRUTH97@users.noreply.github.com>
2020-04-09 13:42:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const config = require('./config/db');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const PORT = Number(process.env.PORT || 3000);
+
+// body parser will receive post data from front end
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set global promise for mongoose
+mongoose.Promise = global.Promise;
+
+// db connection
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  (res) => {
+    console.log('Database sucessfully connected');
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+app.use(cors());
+
+const proRoute = require('./router/router');
+app.use('/product/api', proRoute);
+
+app.listen(PORT, function () {
+  console.log('server is running on url http://localhost:' + PORT);
+});
